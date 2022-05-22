caa92f08215d77e6dbffda2c84db224cfda146b3
GSvenny <gabrielswagner7@gmail.com>
2022-05-21 20:11:44 -0500

    started JavaScript Variables/outline

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//pseudocode thinking
+
+//need variables for start, questions, answers, timer, score, and leaderboard
+
+var startBtn = document.getElementbyId("start");
+var questionsEl = document.getElementById("questions");
+var answersEl = document.getElementById("answers");
+var timerEl = document.getElementbyId("timer");
+var leaderboardEl = document.getElementById("leaderboard");
+
+//--Start Game--//
+
+function startGame {
+    
+}
