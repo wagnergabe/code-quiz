8a54f9e3727b795d9e78c445188e11c8d12ee3f1
GSvenny <gabrielswagner7@gmail.com>
2022-05-22 20:28:53 -0500

    added time and leaderboard layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,26 @@
 
 //need variables for start, questions, answers, timer, score, and leaderboard
 
-var startButtonEl = document.createElement("button");  
-startButtonEl.addEventListener("click", startGame() {
-alert("Lets Go!");
-});
-
-
 startButtonEl.textContent = "Edit";
-var questionsEl = document.getElementById("questions");
+var question = document.querySelector("#questions")
+var trivia = document.querySelectorAll(".trivia-text")
 var answersEl = document.getElementById("answers");
 var timerEl = document.getElementbyId("timer");
 var leaderboardEl = document.getElementById("leaderboard");
 
-//--Start Game--//
+var liveQuestion = {};
+var correctAnswer = true;
+var score = 0;
+var availableQuestions = {};
 
-function startGame {
-alert("Lets Go");
-}
+var questions = [
+    {
+        question: "test test",
+        answer1: "wrong",
+        answer2: "right",
+        answer3: "wrong",
+        answer4: "wrong",
+        answer: 2, 
+    }
+]
+//--Start Game--//
