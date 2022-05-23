e7e30891de2aed4d4085ab6af42866ae2eb2ceaa
GSvenny <gabrielswagner7@gmail.com>
2022-05-22 21:11:52 -0500

    redo functions and added timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,52 @@
 
 //need variables for start, questions, answers, timer, score, and leaderboard
 
-startButtonEl.textContent = "Edit";
-var question = document.querySelector("#questions")
-var trivia = document.querySelectorAll(".trivia-text")
-var answersEl = document.getElementById("answers");
-var timerEl = document.getElementbyId("timer");
-var leaderboardEl = document.getElementById("leaderboard");
+//global variables
 
-var liveQuestion = {};
-var correctAnswer = true;
-var score = 0;
-var availableQuestions = {};
+var timerEl = document.querySelector("p.time");
+var scoreEl = document.querySelector("#player-score");
+var questionEl= document.querySelector("#questions")
+var questionNumber = 0;
+var playerScore = 0;
+
+//--Trivia Questions--//
+
+//--list of items = array--//
 
 var questions = [
     {
-        question: "test test",
-        answer1: "wrong",
-        answer2: "right",
-        answer3: "wrong",
-        answer4: "wrong",
-        answer: 2, 
+    question: "What is an example of Camel-case?",
+    answers: ["1. camelcase", "2. Camelcase" "3. camelCase" "4. Cammmmmel"],
+    correctAnswer: "2"
     }
 ]
-//--Start Game--//
+
+//clock
+
+function countDown() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = `Time:${secondsLeft}s`;
+
+        if (secondsLeft === 0 || questionCount === question.length) {
+            questionEl.style.display = "none";
+            scoreEl.textContent = secondsLeft;
+            }
+        }, 1000);
+}
+
+//Start Game Function (God help me...)
+function startQuiz() {
+    introEl.style.display = "none";
+    questionEl.style.display = "visable";
+    questionCount = 0;
+
+    countDown();
+    setQuestion(questionCount);
+}
+
+var setQuestion = function (id) {
+    questionEl.textContent = question[id].question
+}
+
+//source that helped format questions: https://stackoverflow.com/questions/37252041/storing-quiz-questions-in-array-of-objects
