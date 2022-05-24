6ac72cedda9ee5bb05644354eb399f04144d35f5
GSvenny <gabrielswagner7@gmail.com>
2022-05-23 22:18:51 -0500

    fixed hide display issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,242 +1,94 @@
+//pseudocode thinking
 
-// variables for page elements
-// time and score
-let timeEl = document.querySelector("p.time");
-let secondsLeft = 75;
-let scoreEl = document.querySelector("#score");
+//need variables for start, questions, answers, timer, score, and leaderboard
 
-// sections
-// section intro
-const introEl = document.querySelector("#intro");
+//global variables
 
-// section questions
-//question section
-const questionsEl = document.querySelector("#questions");
-//where question goes
-let questionEl = document.querySelector("#question");
-// how many questions they have answered
-let questionCount = 0;
-// div yaynay
-const yaynayEl = document.querySelector("#yaynay");
 
-// section final
-const finalEl = document.querySelector("#final");
-// user initials
-let initialsInput = document.querySelector("#initials");
+//Well aware Reddit is not a good source, but using querySelector for everything instead of getElmentById/ClassName, seems to work as needed.
+//https://www.reddit.com/r/javascript/comments/5vyf18/is_there_anything_wrong_with_using_queryselector/
 
-// section highscores
-const highscoresEl = document.querySelector("#highscores");
-// ordered list
-let scoreListEl = document.querySelector("#score-list");
-// array of scores
-let scoreList = [];
+var timerEl = document.querySelector("p.time");
+var scoreEl = document.querySelector("#player-score");
+var questionEl= document.querySelector("#questions")
+var playerScore = 0;
+var welcomeEl = document.querySelector("#welcome");
 
-// buttons
-// start
-const startBtn = document.querySelector("#start");
-// answer button class
-const ansBtn = document.querySelectorAll("button.ansBtn")
-// answer1
-const ans1Btn = document.querySelector("#answer1");
-// answer2
-const ans2Btn = document.querySelector("#answer2");
-// answer3
-const ans3Btn = document.querySelector("#answer3");
-// answer4
-const ans4Btn = document.querySelector("#answer4");
-// submit-score
-const submitScrBtn = document.querySelector("#submit-score");
-// goback
-const goBackBtn = document.querySelector("#goback");
-// clearscores
-const clearScrBtn = document.querySelector("#clearscores");
-// view-scores
-const viewScrBtn = document.querySelector("#view-scores");
+var questionEl = document.querySelector("question")
 
-// Object for question, answer, true/false
-const questions = [ // array of objects
+var questionCount = 0;
+
+var rightOrWrong = document.querySelector("#rightOrWrong")
+
+var finalEl = document.querySelector("#final");
+
+var initialsInput = document.querySelector("#initials");
+
+var highscoresEl = document. querySelector("#highscores");
+var scoreListEl = document.querySelector("#score-list");
+
+var scoreList = [];
+
+//buttons
+
+var answer1Btn = document.querySelector("answer1");
+var answer2Btn = document.querySelector("answer2");
+var answer3Btn = document.querySelector("answer3");
+var answer4Btn = document.querySelector("answer4");
+
+var StartBtn = document.querySelector("#start");
+
+var ansBtn = document.querySelectorAll("button.ansBtn");
+
+
+var submitScrBtn = document.querySelector("#submit-score")
+
+//questions
+
+var questions = [
     {
-        // question 0
-        question: "Commonly used data types do NOT include:",
-        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        correctAnswer: "2"
-    },
-    {
-        // question 1
-        question: "The condition in an if / else statement is enclosed within ____.",
-        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-        correctAnswer: "1"
-    },
-    {
-        // question 2
-        question: "Arrays in Javascript can be used to store ____.",
-        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-        correctAnswer: "3"
-    },
-    {
-        // question 3
-        question: "String values must be enclosed within ____ when being assigned to variables.",
-        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
-        correctAnswer: "2"
-    },
-    {
-        // question 4
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
-        correctAnswer: "3"
+    question: "What is an example of Camel-case?",
+    answers: ["1. camelcase", "2. Camelcase", "3 camelCase", "4. Cammmmmel"],
+    correctAnswer: "2"
     }
-];
+]
 
+//source that helped format questions: https://stackoverflow.com/questions/37252041/storing-quiz-questions-in-array-of-objects
 
-// Functions
+//clock
 
-// timer
-function setTime() {
-    let timerInterval = setInterval(function () {
+function countDown() {
+    var timerEl = setInterval(function () {
         secondsLeft--;
-        timeEl.textContent = `Time:${secondsLeft}s`;
+        timerEl.textContent = `Time:${secondsLeft}s`;
 
-        if (secondsLeft === 0 || questionCount === questions.length) {
-            clearInterval(timerInterval);
-            questionsEl.style.display = "none";
-            finalEl.style.display = "block";
+        if (secondsLeft === 0 || questionCount === question.length) {
+            questionEl.style.display = "none";
             scoreEl.textContent = secondsLeft;
-        }
-    }, 1000);
+            }
+        }, 1000);
 }
 
-// start quiz with timer and set up questions
-function startQuiz() {
-    introEl.style.display = "none";
-    questionsEl.style.display = "block";
+//Start Game Function (God help me...)
+function startGame() {
+    welcomeEl.style.display = "none";
+    questionEl.style.display = "visable";
     questionCount = 0;
 
-    setTime();
+    countDown();
     setQuestion(questionCount);
 }
 
-// function to set question; takes in a count and displays the next question/answers
-function setQuestion(id) {
-    if (id < questions.length) {
-        questionEl.textContent = questions[id].question;
-        ans1Btn.textContent = questions[id].answers[0];
-        ans2Btn.textContent = questions[id].answers[1];
-        ans3Btn.textContent = questions[id].answers[2];
-        ans4Btn.textContent = questions[id].answers[3];
-    }
+
+
+var setQuestion = function (id) {
+    if(id < questions.length) {
+    questionEl.textContent = questions[id].question;
+    ans1Btn.textContent = questions[id].answer [0];
+    ans2Btn.textContent = questions[id].answer [1];
+    ans3Btn.textContent = questions[id].answer [2];
+    ans4Btn.textContent = questions[id].answer [3];
+  }
 }
 
-// function to check answer and then move to next question
-function checkAnswer(event) {
-    event.preventDefault();
 
-    // show section for yaynay and append message
-    yaynayEl.style.display = "block";
-    let p = document.createElement("p");
-    yaynayEl.appendChild(p);
-
-    // time out after 1 second
-    setTimeout(function () {
-        p.style.display = 'none';
-    }, 1000);
-
-    // answer checker
-    if (questions[questionCount].correctAnswer === event.target.value) {
-        p.textContent = "Correct!";
-    } else if (questions[questionCount].correctAnswer !== event.target.value) {
-        secondsLeft = secondsLeft - 10;
-        p.textContent = "Wrong!";
-    }
-
-    // increment so the questions index is increased
-    if (questionCount < questions.length) {
-        questionCount++;
-    }
-    // call setQuestion to bring in next question when any ansBtn is clicked
-    setQuestion(questionCount);
-}
-
-function addScore(event) {
-    event.preventDefault();
-
-    finalEl.style.display = "none";
-    highscoresEl.style.display = "block";
-
-    let init = initialsInput.value.toUpperCase();
-    scoreList.push({ initials: init, score: secondsLeft });
-
-    // sort scores
-    scoreList = scoreList.sort((a, b) => {
-        if (a.score < b.score) {
-          return 1;
-        } else {
-          return -1;
-        }
-      });
-    
-    scoreListEl.innerHTML="";
-    for (let i = 0; i < scoreList.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = `${scoreList[i].initials}: ${scoreList[i].score}`;
-        scoreListEl.append(li);
-    }
-
-    // Add to local storage
-    storeScores();
-    displayScores();
-}
-
-function storeScores() {
-    localStorage.setItem("scoreList", JSON.stringify(scoreList));
-}
-
-function displayScores() {
-    // Get stored scores from localStorage
-    // Parsing the JSON string to an object
-    let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
-
-    // If scores were retrieved from localStorage, update the scorelist array to it
-    if (storedScoreList !== null) {
-        scoreList = storedScoreList;
-    }
-}
-
-// clear scores
-function clearScores() {
-    localStorage.clear();
-    scoreListEl.innerHTML="";
-}
-
-// EventListeners
-// Start timer and display first question when click start quiz
-startBtn.addEventListener("click", startQuiz);
-
-// Check answers loop
-ansBtn.forEach(item => {
-    item.addEventListener('click', checkAnswer);
-});
-
-// Add score
-submitScrBtn.addEventListener("click", addScore);
-
-// Go Back Button
-goBackBtn.addEventListener("click", function () {
-    highscoresEl.style.display = "none";
-    introEl.style.display = "block";
-    secondsLeft = 75;
-    timeEl.textContent = `Time:${secondsLeft}s`;
-});
-
-// Clear the scores
-clearScrBtn.addEventListener("click", clearScores);
-
-// View/Hide High Scores Button
-viewScrBtn.addEventListener("click", function () {
-    if (highscoresEl.style.display === "none") {
-        highscoresEl.style.display = "block";
-    } else if (highscoresEl.style.display === "block") {
-        highscoresEl.style.display = "none";
-    } else {
-        return alert("No scores to show.");
-    }
-});
